1708d89f2047814229b0872bc39d4235df084490
SatoshiHimura <h.satoshi.0722@outlook.jp>
2017-02-25 23:52:08 +0900

    小文字を大文字に

---
+++ test 3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String sk = sc.nextLine();
+        String s = sk.toUpperCase();
+        System.out.println(s);
+    }
+}

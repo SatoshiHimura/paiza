fb820cec556d39fa094846385a3bc437a262a9ce
SatoshiHimura <h.satoshi.0722@outlook.jp>
2017-02-25 23:48:39 +0900

    通知表

---
+++ test 2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        switch(N){
+            case 1:
+                System.out.println("E");
+                break;
+            case 2:
+                System.out.println("D");
+                break;
+            case 3:
+                System.out.println("C");
+                break;
+            case 4:
+                System.out.println("B");
+                break;
+            case 5:
+                System.out.println("A");
+                break;
+        }
+    }
+}
